cbe76febbba85985bd71ef77304b347ed274d1c6
hasnat.jamil <hasnat.jamil@enosisbd.com>
2025-07-23 00:28:57 +0600

    boilerplate setup

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div className="bg-slate-700 w-full h-screen text-gray-200">
+      <h1 className="text-4xl font-bold text-center">
+        Welcome to github actions learning
+      </h1>
+      <p className="text-center">
+        This is a simple app to learn github actions.
+      </p>
+      <p className="text-center mt-4">
+        <a
+          href="https://github.com/actions"
+          className="text-pink-400 underline"
+        >
+          Github Actions
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default App;
